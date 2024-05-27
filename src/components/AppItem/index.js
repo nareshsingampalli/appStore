a0c68dfc5c530e05bcffc01fdea2cbcebfe25f0e
nareshsingampalli <vgnsingampalli@gmail.com>
2024-05-27 14:37:29 +0000

    first commit

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,5 @@
+import './index.css'
+
+const AppItem = props => <h1>App Item</h1>
+
+export default AppItem
---
+++ src/components/TabItem/index.js
@@ -0,0 +1,5 @@
+import './index.css'
+
+const TabItem = props => <h1>TabItem</h1>
+
+export default TabItem
